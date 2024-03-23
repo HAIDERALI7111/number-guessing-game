c1b57f3f09e811f4833428f871af51836f04e7ab
HAIDERALI7111 <159697057+HAIDERALI7111@users.noreply.github.com>
2024-03-24 03:54:04 +0500

    npx number guess

---
+++ game.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 6) + 1;
+const answer = await inquirer.prompt({
+    name: "randomNumber",
+    type: "number",
+    message: "guess the number"
+});
+if (answer.randomNumber === randomNumber) {
+    console.log("Congratulations your guessing number is correct");
+}
+else {
+    console.log(" your guessing number is wrong please try again!");
+}
